640d5f62e2d19d072238d415f8b1954ec95a80ff
niship2 <48500095+niship2@users.noreply.github.com>
2023-12-21 08:09:02 +0000

    modify:色々機能追加

--- Hello.py
+++ Hello.py
@@ -1,81 +1,101 @@
-# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import streamlit as st
 import pandas as pd
-#import xlsxwriter
+import networkx as nx
+
+# import xlsxwriter
 
 
 from streamlit.logger import get_logger
+from draw_net import draw_net
 
 LOGGER = get_logger(__name__)
 
 
+color_discrete_map = {
+    "pink": "pink",
+    "darksalmon": "darksalmon",
+}
+
+
 def run():
-    st.set_page_config(
-        page_title="Hello",
-        page_icon="👋",
-        
-    )
+    st.set_page_config(page_title="Hello", page_icon=":smiley:", layout="wide")
 
-    st.title("競合・競合先探索アプリ！")
+    
     st.write("サイドバーから分析対象ファイルをアップロードしてください。")
-
+    # st.sidebar.warning("アップロードファイルはサーバには保存されないので注意")
 
     # ファイルアップロードウィジェット
     uploaded_file = st.sidebar.file_uploader("ファイルを選択", type=["csv", "xlsx"])
+
+    df = pd.DataFrame()
 
     # ファイルがアップロードされたら、その内容を読み込む
     if uploaded_file is not None:
         # ファイルの拡張子によって読み込み方法を分岐
         if uploaded_file.name.endswith(".csv"):
-          df = pd.read_csv(uploaded_file,encoding="cp932")
+            df = pd.read_csv(uploaded_file, encoding="cp932")
         elif uploaded_file.name.endswith(".xlsx"):
-          df = pd.read_excel(uploaded_file)
-      
-    if df is not None:
+            df = pd.read_excel(uploaded_file)
+
+    if df.shape[0] != 0:
         with st.expander("読み込んだデータ"):
-        # データフレームを表示
-            st.dataframe(df)            
-            
-    sep_list = [",","|"]
-    with st.sidebar.form("my_form"):
+            # データフレームを表示
+            st.dataframe(df)
+
+    sep_list = [",", "|"]
+    with st.sidebar.expander("出願人列と分類列指定"):
         all_col = df.columns.tolist()
-        applicant_col = st.selectbox("出願人の列を選択",all_col)
-        sep1 = st.selectbox("共同出願人の場合の区切り",sep_list)
+        applicant_col = st.selectbox("出願人の列を選択", all_col)
+        sep1 = st.selectbox("共同出願人の場合の区切り", sep_list)
 
-        #all_col.remove(applicant_col)
+        # all_col.remove(applicant_col)
 
-        clas_col = st.selectbox("特許分類の列を選択",all_col)
-        sep2 = st.selectbox("複数の特許分類の区切り",sep_list)
-
+        clas_col = st.selectbox("特許分類の列を選択", all_col)
+        sep2 = st.selectbox("複数の特許分類の区切り", sep_list)
 
         # Every form must have a submit button.
-        submitted = st.form_submit_button("Submit")
+        # submitted = st.form_submit_button("Submit")
 
-    if submitted:
-        left_df = df[applicant_col].str.split(sep1,expand=True).stack().reset_index()
-        right_df = df[clas_col].str.split(sep2,expand=True).stack().reset_index()
+    if True:
+        left_df = df[applicant_col].str.split(sep1, expand=True).stack().reset_index()
+        right_df = df[clas_col].str.split(sep2, expand=True).stack().reset_index()
 
-        #同じindex同士合体
-        decomp_df = pd.merge(left_df,right_df,on="level_0")
-        decomp_df = decomp_df[["0_x","0_y"]].rename(columns={"0_x":applicant_col,"0_y":clas_col})
-            
+        # 同じindex同士合体
+        decomp_df = pd.merge(left_df, right_df, on="level_0")
+        decomp_df = decomp_df[["0_x", "0_y"]].rename(
+            columns={"0_x": applicant_col, "0_y": clas_col}
+        )
 
         with st.expander("出願人列と分類列を分解"):
-            agg_df = decomp_df.groupby([applicant_col,clas_col],as_index=False,dropna=False).size()
-            st.dataframe(agg_df.sort_values(by="size",ascending=False))
+            agg_df = decomp_df.groupby(
+                [applicant_col, clas_col], as_index=False, dropna=False
+            ).size()
+            st.dataframe(agg_df.sort_values(by="size", ascending=False))
+
+        with st.expander("ネットワーク図"):
+            select_list = df[applicant_col].value_counts().index.tolist()[20:29]
+            agg_df2 = agg_df[agg_df[applicant_col].isin(select_list)]
+            applicant_label_list = agg_df2["出願人・権利者名"].tolist()
+            clas_label_list = agg_df2["IPC"].tolist()
+
+            G = nx.from_pandas_edgelist(agg_df2, target=applicant_col, source=clas_col)
+
+            # 描画パラメータ
+            draw_param_col1, draw_param_col2, draw_param_col3 = st.columns(3)
+            with draw_param_col1:
+                NODE_SIZE = st.slider("円の大きさ", 5, 100, 10)
+                EDGE_WIDTH = st.slider("線の太さ", 0.1, 5.0, 0.2)
+            with draw_param_col2:
+                k = st.slider("ばね定数", 0.01, 3.0, 0.15)
+                scale = st.slider("スケール", 1, 1000, 500)
+            with draw_param_col3:
+                label_flag = st.checkbox("ラベル表示")
+
+            fig = draw_net(
+                G, applicant_label_list, NODE_SIZE, EDGE_WIDTH, k, scale, label_flag
+            )
+
+            st.plotly_chart(fig, use_container_width=True)
 
 
 if __name__ == "__main__":
---
+++ draw_net.py
@@ -0,0 +1,109 @@
+import plotly.express as px
+import pandas as pd
+import numpy as np
+import plotly.graph_objects as go
+import networkx as nx
+
+
+def set_shape(label, applicant_label_list):
+    if label in applicant_label_list:
+        return "square"
+    else:
+        return "circle"
+
+
+def set_color(label, applicant_label_list):
+    if label in applicant_label_list:
+        return "pink"
+    else:
+        return "green"
+
+
+def draw_net(
+    G,
+    applicant_label_list,
+    NODE_SIZE=10,
+    EDGE_WIDTH=0.2,
+    k=0.15,
+    scale=1000,
+    label_flag=False,
+):
+    pos = nx.spring_layout(
+        G,
+        iterations=20,
+        scale=scale,
+        k=k,
+        weight="size",
+        seed=1,
+    )
+
+    draw_df = (
+        pd.DataFrame.from_dict(pos, orient="index", columns=["x", "y"])
+        .reset_index()
+        .reset_index()
+        .rename(columns={"level_0": "id", "index": "label"})
+    )
+    draw_df["shape"] = draw_df["label"].apply(
+        lambda x: set_shape(x, applicant_label_list)
+    )
+    draw_df["color"] = draw_df["label"].apply(
+        lambda x: set_color(x, applicant_label_list)
+    )
+
+    nodejson = draw_df.to_dict(orient="records")
+
+    # G2でidとlabelを対応させる。
+    G2 = nx.convert_node_labels_to_integers(
+        G, first_label=0, ordering="default", label_attribute="nodelabel"
+    )
+    edge_x = []
+    edge_y = []
+    for edge in G2.edges():
+        # print(nodejson[edge[0]]["x"])
+        x0 = nodejson[edge[0]]["x"]
+        y0 = nodejson[edge[0]]["y"]
+        x1 = nodejson[edge[1]]["x"]
+        y1 = nodejson[edge[1]]["y"]
+        edge_x.append(x0)
+        edge_x.append(x1)
+        edge_x.append(None)
+        edge_y.append(y0)
+        edge_y.append(y1)
+        edge_y.append(None)
+
+    fig = go.Figure(
+        data=[
+            go.Scattergl(
+                x=edge_x,
+                y=edge_y,
+                mode="lines",
+                text=draw_df["label"].tolist(),
+            ),
+        ]
+    )
+
+    fig.add_scatter(
+        x=draw_df["x"],
+        y=draw_df["y"],
+        mode="markers",
+        text=draw_df["label"],
+        hoverinfo="text",
+    )
+
+    fig.update_traces(
+        line={"width": EDGE_WIDTH, "color": "grey"},
+    )
+
+    fig.update_traces(
+        marker=dict(
+            size=NODE_SIZE, symbol=draw_df["shape"], color=draw_df["color"], angle=0
+        ),
+        selector=dict(mode="markers"),
+    )
+
+    # fig.update_layout(dragmode='drawrect')
+    fig.update_layout(xaxis=dict(showgrid=False, showticklabels=False))  # 軸のグリッドを表示
+    fig.update_layout(yaxis=dict(showgrid=False, showticklabels=False))  # 軸のグリッドを表示
+    fig.update_layout(title="出願人と分野のネットワーク図\n<sub>■は出願人、〇は分野</sub>")
+
+    return fig
